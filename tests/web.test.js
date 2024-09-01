7877d2357c24e6a0f24088ccfb955442bd7926d3
ikkyuuq <kittipongprasompong@gmail.com>
2024-09-01 09:46:33 +0700

    delete tests dir

--- tests/web.test.js
+++
@@ -1,7 +0,0 @@
-import { describe, it } from "node:test";
-
-describe("Web app test suite", () => {
-  it("given valid input, should update the table data", async () => {
-    console.log("working");
-  });
-});
